695ce0c64783f54572c8ad90e15a4ea94dea8a47
Chris McCormick <chris@mccormick.cx>
2024-07-14 22:40:51 +0100

    Correctly sending DALL-E requests.

--- src/makesprite/core.cljs
+++ src/makesprite/core.cljs
@@ -1,23 +1,20 @@
 (ns makesprite.core
   (:require
     #_ [applied-science.js-interop :as j]
-    #_ [promesa.core :as p]
+    [promesa.core :as p]
     [reagent.core :as r]
     [reagent.dom :as rdom]
     [shadow.resource :as rc]
     [alandipert.storage-atom :refer [local-storage]]
     #_ ["openai" :as openai]))
 
-(defn initial-state []
-  {:inflight {}
-   :settings {:openai-key nil}
-   :ui {}})
-
 ;curl https://api.openai.com/v1/images/generations \
 ;  -H "Content-Type: application/json" \
 ;  -H "Authorization: Bearer $OPENAI_API_KEY" \
 
 ; *** constants *** ;
+
+(def api-url "https://api.openai.com")
 
 (def re-html-comment (js/RegExp. "<\\!--.*?-->" "g"))
 
@@ -28,13 +25,20 @@
 (def placeholder-prompt
   "A collection of casual hand-drawn game sprites for a roguelike game, featuring a new set of fantasy characters with even simpler bodies and limbs. These characters are emphasized with a very doodly, sketchy hand-drawn style, using a muted and soft color palette. The characters are designed with extremely simplified chibi-style proportions, featuring larger heads, very minimalistic bodies, and almost stick-figure-like limbs, depicted in front-facing poses on a pure white background. The emphasis is on making the sprites whimsically minimalistic, maintaining consistency with the game's visual theme.")
 
-(def api-dall-e
+(def api-req-dall-e
   {:model "dall-e-3"
    :prompt ""
    :n 1
    :size "1792x1024"})
 
 ; *** functions *** ;
+
+; simply a history of events e.g. sends, inflights, etc.?
+(defn initial-state []
+  {:inflight {}
+   :settings {:openai-key nil}
+   :ui {:prompt placeholder-prompt}
+   :log []})
 
 (defn now [] (-> (js/Date.) .toISOString))
 
@@ -46,8 +50,42 @@
        (> (aget k "length") 45)))
 
 (defn initiate-request [state]
-  (let [text (or (get-in @state [:ui :prompt]) placeholder-prompt)]
-    (js/console.log text)))
+  (let [text (str
+               prompt-dall-e-strict "\n"
+               (get-in @state [:ui :prompt]))
+        payload (assoc api-req-dall-e
+                       :prompt text
+                       :metadata {:id (make-id)
+                                  :k :dall-e-request
+                                  :t (now)})
+        api-key (get-in @state [:settings :openai-key])]
+    (swap! state
+           #(-> %
+                (assoc :inflight payload)
+                (update-in [:log] conj payload)))
+    (p/let [req (js/fetch
+                  (str api-url "/v1/images/generations")
+                  #js {:method "POST"
+                       :headers #js {:Content-Type "application/json"
+                                     :Authorization (str "Bearer " api-key)}
+                       :body (-> payload
+                                 (dissoc :metadata)
+                                 clj->js
+                                 js/JSON.stringify)})
+            result (.json req)
+            result (js->clj result :keywordize-keys true)
+            result (assoc result
+                          :metadata
+                          {:id (make-id)
+                           :parent (get-in payload [:metadata :id])
+                           :k :dall-e-response
+                           :t (now)})]
+      (js/console.log result)
+      (swap! state
+             #(-> %
+                  (dissoc :inflight)
+                  (update-in [:ui] dissoc :prompt)
+                  (update-in [:log] conj result))))))
 
 ; *** components *** ;
 
@@ -60,19 +98,32 @@
 ; *** views & event handlers ***;
 
 (defn component:prompt [state]
-  (let [txt (or (get-in @state [:ui :prompt]) placeholder-prompt)]
+  (let [txt (get-in @state [:ui :prompt])]
      [:textarea
       {:rows 10
+       :placeholder "Enter your game sprite prompt here..."
        :on-change #(swap! state
                           assoc-in [:ui :prompt]
                           (-> % .-target .-value))
        :value txt}]))
 
 (defn component:main [state]
-  [:main
-   [component:prompt state (r/atom nil)]
-   [:button {:on-click #(initiate-request state)}
-    "send"]])
+  (let [openai-key (get-in @state [:settings :openai-key])]
+    [:main
+     [:input {:placeholder "Enter OpenAI API key..."
+              :value openai-key
+              :read-only (seq (:inflight @state))
+              :on-change #(swap! state assoc-in [:settings :openai-key]
+                                 (-> % .-target .-value))
+              :class (when (not (is-valid-key? openai-key)) "warning")}]
+     [component:prompt state (r/atom nil)]
+     [:button {:on-click #(initiate-request state)
+               :disabled (or (empty? (get-in @state [:ui :prompt]))
+                             (not (is-valid-key? openai-key))
+                             (seq (:inflight @state)))}
+      "send"]
+     (when (seq (:inflight @state))
+       [:p "LOADING..."])]))
 
 (defn component:header []
   [:header
